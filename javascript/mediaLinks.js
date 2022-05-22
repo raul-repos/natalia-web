2189c886a84b87f9f0066a40d2a1b1c12f2db229
Raul-Docs <raul.cal.bar@gmail.com>
2022-05-22 20:12:42 +0200

    Version ordenador terminada

--- javascript/mediaLinks.js
+++ javascript/mediaLinks.js
@@ -1,7 +1,7 @@
 const facebookClick = () => {
-  window.open("https://es-es.facebook.com/")
+  window.open("https://www.facebook.com/search/top?q=centro%20crealia")
 }
 
 function instagramClick() {
-  window.open("https://www.instagram.com/?hl=es")
+  window.open("https://www.instagram.com/centrocrealia/")
 }
